e4b94341010baeb3f0b7e6a39364283627284465
shresthdubey98 <shresthdubey98@gmail.com>
2019-06-24 12:26:13 +0530

    all ui element updates are moved under RunOnUiThread to resolve app crash

--- app/src/main/java/com/example/navigationwithtoolbar/HomeActivity.kt
+++ app/src/main/java/com/example/navigationwithtoolbar/HomeActivity.kt
@@ -2,16 +2,14 @@ package com.example.navigationwithtoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-
     private lateinit var navControler :NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
